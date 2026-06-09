057bccde0e4c3df6f4778de0e71ef71f67b5a13d
0kAj <122619846+0kAj@users.noreply.github.com>
2026-06-09 16:43:22 +0200

    refactoring

--- src/app/page/snake-game/models/vector2.ts
+++ src/app/page/snake-game/models/vector2.ts
@@ -1,4 +1,26 @@
-export interface Vector2 {
-  x: number;
-  y: number;
+export class Vector2 {
+  constructor(
+    public x: number,
+    public y: number,
+  ) {}
+
+  addVector(b: Vector2): Vector2 {
+    return new Vector2(this.x + b.x, this.y + b.y);
+  }
+
+  sameVector(b: Vector2): boolean {
+    return this.x === b.x && this.y === b.y;
+  }
+
+  reverseVector(b: Vector2): boolean {
+    return this.x === -b.x && this.y === -b.y;
+  }
+
+  toString(): string {
+    return `Vector2(${this.x}, ${this.y})`;
+  }
+
+  toKey(): string {
+    return `${this.x}:${this.y}`;
+  }
 }
