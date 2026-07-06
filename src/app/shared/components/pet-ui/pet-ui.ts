f27f47c2737d4c7f0e07a09721b242c8ff8a5fde
0kAj <122619846+0kAj@users.noreply.github.com>
2026-07-06 14:58:22 +0200

    adding feed animation

--- src/app/shared/components/pet-ui/pet-ui.ts
+++ src/app/shared/components/pet-ui/pet-ui.ts
@@ -11,9 +11,17 @@ export class PetUi {
   @Input() show: boolean = false;
   @Output() close: EventEmitter<void> = new EventEmitter<void>();
 
+  feeding = false;
+
   constructor(protected statManager: StatManager) {}
 
   feed() {
     this.statManager.eatScore(10);
+
+    //play feed animation
+    this.feeding = true;
+    setTimeout(() => {
+      this.feeding = false;
+    }, 500);
   }
 }
